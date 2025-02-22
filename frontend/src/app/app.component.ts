c9cfb6344ffeef30978dd178fbb7f777c3f04c1b
Marcel Hendrich <inpercima@gmail.com>
2025-02-22 15:04:20 +0100

    chore(deps): update tailwind to 4.0.8

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component, TemplateRef, inject } from '@angular/core';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { Title } from '@angular/platform-browser';
-
 import { DOCUMENT } from '@angular/common';
+import { Component, TemplateRef, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { Title } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 import { DashboardComponent } from './features/dashboard/dashboard.component';
 
